4cc0826a8bbbab95a701b6fcb65fe5b6b942020a
rosieliu <rosieliu@lexin.com>
2020-03-06 17:15:14 +0800

    外层结构调整

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -2,54 +2,53 @@
   <div id="app">
     <top-header></top-header>
     <el-container>
-      <div class="body-ctn">
+        <el-aside width="250px">
+          <div class="directory">
+            <el-col :span="24">
+              <h5>目录</h5>
+              <el-menu
+                default-active="/"
+                class="el-menu-vertical-demo"
+                router>
+                <el-menu-item index="/">
+                  <i class="el-icon-menu"></i>
+                  <span slot="title">首页</span>
+                </el-menu-item>
+                <el-submenu index="2">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>组织</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="/communityCreate">组织创建</el-menu-item>
+                    <el-menu-item index="/communityManage">组织管理</el-menu-item>
+                  </el-menu-item-group>
+                </el-submenu>
+                <el-submenu index="3">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>活动</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="/activityCreate">活动创建</el-menu-item>
+                    <el-menu-item index="/activityManage">活动管理</el-menu-item>
+                  </el-menu-item-group>
+                </el-submenu>
+                <el-submenu index="4">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>审批中心</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="/communityApproval">社团审批</el-menu-item>
+                    <el-menu-item index="/activityApproval">活动审批</el-menu-item>
+                  </el-menu-item-group>
+                </el-submenu>
+              </el-menu>
+            </el-col>
+          </div>
+        </el-aside>
         <el-container>
-          <el-aside width="250px">
-            <div class="directory">
-              <el-col :span="24">
-                <h5>目录</h5>
-                <el-menu
-                  default-active="/"
-                  class="el-menu-vertical-demo"
-                  router>
-                  <el-menu-item index="/">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">首页</span>
-                  </el-menu-item>
-                  <el-submenu index="2">
-                    <template slot="title">
-                      <i class="el-icon-location"></i>
-                      <span>组织</span>
-                    </template>
-                    <el-menu-item-group>
-                      <el-menu-item index="/communityCreate">组织创建</el-menu-item>
-                      <el-menu-item index="/communityManage">组织管理</el-menu-item>
-                    </el-menu-item-group>
-                  </el-submenu>
-                  <el-submenu index="3">
-                    <template slot="title">
-                      <i class="el-icon-location"></i>
-                      <span>活动</span>
-                    </template>
-                    <el-menu-item-group>
-                      <el-menu-item index="/activityCreate">活动创建</el-menu-item>
-                      <el-menu-item index="/activityManage">活动管理</el-menu-item>
-                    </el-menu-item-group>
-                  </el-submenu>
-                  <el-submenu index="4">
-                    <template slot="title">
-                      <i class="el-icon-location"></i>
-                      <span>审批中心</span>
-                    </template>
-                    <el-menu-item-group>
-                      <el-menu-item index="/communityApproval">社团审批</el-menu-item>
-                      <el-menu-item index="/activityApproval">活动审批</el-menu-item>
-                    </el-menu-item-group>
-                  </el-submenu>
-                </el-menu>
-              </el-col>
-            </div>
-          </el-aside>
           <el-main>
             <div class="main">
               <keep-alive>
@@ -58,22 +57,22 @@
             </div>
           </el-main>
         </el-container>
-      </div>
     </el-container>
   </div>
 </template>
 
 <script>
-  import { TopHeader } from './components/common/index.js'
-  export default {
-    name: 'App',
-    components: {
-      'top-header': TopHeader
-    }
-  };
+import { TopHeader } from './components/common/index.js';
+
+export default {
+  name: 'App',
+  components: {
+    'top-header': TopHeader,
+  },
+};
 </script>
 
-<style> 
+<style>
   * {
     margin: 0;
   }
